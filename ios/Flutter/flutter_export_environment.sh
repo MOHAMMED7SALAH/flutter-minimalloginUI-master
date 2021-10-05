6c0cb27af47791709ea26d7db99833c058621b78
MOHAMMED7SALAH <bentitimohammedsalahstars7@gmail.com>
2021-10-05 16:31:02 +0100

    login_app

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=D:\Programmes\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\MOHAMMED SALAH\Downloads\flutter-minimalloginUI-master\flutter-minimalloginUI-master"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
